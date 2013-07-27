6d9a764e7d6935abf4bc0c8c8e6d3010ffc81da7
Takumi <takumi@themeboy.com>
2013-07-27 14:13:21 +1000

    Select tab bar by class instead of id

--- sportspress-admin.js
+++ sportspress-admin.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
-	$('#sportspress-tabs a').click(function() {
+	$('.sp-tab-panel').siblings('.wp-tab-bar').find('a').click(function() {
 		$(this).closest('li').removeClass('wp-tab').addClass('wp-tab-active').siblings().removeClass('wp-tab-active').addClass('wp-tab').closest('.wp-tab-bar').siblings($(this).attr('href')).show().siblings('.wp-tab-panel').hide();
 		return false;
 	});
