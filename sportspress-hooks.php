d12c24269ee7ae7833c3f8057a76e84419a705a4
ThemeBoy <takumi@Takumis-Mac-mini.local>
2013-12-04 05:22:12 +1100

    Initialize custom post types before flushing rewrite rules on activation

--- sportspress-hooks.php
+++ sportspress-hooks.php
@@ -1,7 +1,12 @@
 <?php
 // Flush rewrite rules on activation to make sure permalinks work properly
 function sp_rewrite_flush() {
+    sp_event_cpt_init();
     sp_team_cpt_init();
+    sp_table_cpt_init();
+    sp_player_cpt_init();
+    sp_list_cpt_init();
+    sp_staff_cpt_init();
     flush_rewrite_rules();
 }
 register_activation_hook( __FILE__, 'sp_rewrite_flush' );
