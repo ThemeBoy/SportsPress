900bebee72542faebbfa70c7dccecc7ca234ce82
Brian Miyaji <brian@themeboy.com>
2013-12-31 05:01:33 +1100

    Fix baseball presets

--- presets/baseball.php
+++ presets/baseball.php
@@ -39,10 +39,6 @@ $sportspress_sports['baseball'] = array(
 				'post_name'  => 'win'
 			),
 			array(
-				'post_title' => __( 'Draw', 'sportspress' ),
-				'post_name'  => 'draw'
-			),
-			array(
 				'post_title' => __( 'Loss', 'sportspress' ),
 				'post_name'  => 'loss'
 			)
@@ -50,34 +46,64 @@ $sportspress_sports['baseball'] = array(
 		// Results
 		'sp_result' => array(
 			array(
-				'post_title' => __( 'Goals', 'sportspress' ),
-				'post_name'  => 'goals'
+				'post_title' => __( '1', 'sportspress' ),
+				'post_name'  => 'one'
 			),
 			array(
-				'post_title' => __( '1st Half', 'sportspress' ),
-				'post_name'  => 'firsthalf'
+				'post_title' => __( '2', 'sportspress' ),
+				'post_name'  => 'two'
 			),
 			array(
-				'post_title' => __( '2nd Half', 'sportspress' ),
-				'post_name'  => 'secondhalf'
+				'post_title' => __( '3', 'sportspress' ),
+				'post_name'  => 'three'
+			),
+			array(
+				'post_title' => __( '4', 'sportspress' ),
+				'post_name'  => 'four'
+			),
+			array(
+				'post_title' => __( '5', 'sportspress' ),
+				'post_name'  => 'five'
+			),
+			array(
+				'post_title' => __( '6', 'sportspress' ),
+				'post_name'  => 'six'
+			),
+			array(
+				'post_title' => __( '7', 'sportspress' ),
+				'post_name'  => 'seven'
+			),
+			array(
+				'post_title' => __( '8', 'sportspress' ),
+				'post_name'  => 'eight'
+			),
+			array(
+				'post_title' => __( '9', 'sportspress' ),
+				'post_name'  => 'nine'
+			),
+			array(
+				'post_title' => __( '&nbsp;', 'sportspress' ),
+				'post_name'  => 'nbsp'
+			),
+			array(
+				'post_title' => __( 'R', 'sportspress' ),
+				'post_name'  => 'r'
+			),
+			array(
+				'post_title' => __( 'H', 'sportspress' ),
+				'post_name'  => 'h'
+			),
+			array(
+				'post_title' => __( 'E', 'sportspress' ),
+				'post_name'  => 'e'
 			)
 		),
 		// Columns
 		'sp_column' => array(
 			array(
-				'post_title' => __( 'P', 'sportspress' ),
-				'post_name'  => 'p',
-				'meta'       => array( 'sp_equation' => '$eventsplayed' )
-			),
-			array(
 				'post_title' => __( 'W', 'sportspress' ),
 				'post_name'  => 'w',
 				'meta'       => array( 'sp_equation' => '$win' )
-			),
-			array(
-				'post_title' => __( 'D', 'sportspress' ),
-				'post_name'  => 'd',
-				'meta'       => array( 'sp_equation' => '$draw' )
 			),
 			array(
 				'post_title' => __( 'L', 'sportspress' ),
@@ -85,24 +111,19 @@ $sportspress_sports['baseball'] = array(
 				'meta'       => array( 'sp_equation' => '$loss' )
 			),
 			array(
-				'post_title' => __( 'F', 'sportspress' ),
-				'post_name'  => 'f',
-				'meta'       => array( 'sp_equation' => '$goalsfor', 'sp_priority' => '3', 'sp_order' => 'DESC' )
+				'post_title' => __( 'PCT', 'sportspress' ),
+				'post_name'  => 'pct',
+				'meta'       => array( 'sp_equation' => '$win / $eventsplayed' )
 			),
 			array(
-				'post_title' => __( 'A', 'sportspress' ),
-				'post_name'  => 'a',
-				'meta'       => array( 'sp_equation' => '$goalsagainst' )
+				'post_title' => __( 'GB', 'sportspress' ),
+				'post_name'  => 'gb',
+				'meta'       => array( 'sp_equation' => '( $winmax + $loss - $win - $lossmax ) / 2' )
 			),
 			array(
-				'post_title' => __( 'GD', 'sportspress' ),
-				'post_name'  => 'gd',
-				'meta'       => array( 'sp_equation' => '$goalsfor - $goalsagainst', 'sp_priority' => '2', 'sp_order' => 'DESC' )
-			),
-			array(
-				'post_title' => __( 'PTS', 'sportspress' ),
-				'post_name'  => 'pts',
-				'meta'       => array( 'sp_equation' => '$win * 3 + $draw', 'sp_priority' => '1', 'sp_order' => 'DESC' )
+				'post_title' => __( 'STRK', 'sportspress' ),
+				'post_name'  => 'strk',
+				'meta'       => array( 'sp_equation' => '$streak' )
 			)
 		)
 	)
