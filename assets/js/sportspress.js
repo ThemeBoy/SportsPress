c6df9ebca013e410c8a23eb12130fa1e88d51d98
Brian Miyaji <brian@themeboy.com>
2014-01-09 16:15:15 +1100

    Make number column sortable ascending only

--- assets/js/sportspress.js
+++ assets/js/sportspress.js
@@ -1,21 +1,21 @@
 (function($) {
 
 	// Data tables
-	$('.sp-data-table').dataTable({
-		'aaSorting': [],
-		'bAutoWidth': false,
-		'bFilter': false,
-		'bInfo': false,
-		'bPaginate': false,
-		'bSort': true,
-	    'oLanguage': {
-	      'oAria': {
-	        'sSortAscending': '',
-	        'sSortDescending': ''
+	$(".sp-data-table").dataTable({
+		"aaSorting": [],
+		"bAutoWidth": false,
+		"bFilter": false,
+		"bInfo": false,
+		"bPaginate": false,
+		"bSort": true,
+	    "oLanguage": {
+	      "oAria": {
+	        "sSortAscending": "",
+	        "sSortDescending": ""
 	      }
 	    },
-		'aoColumnDefs': [
-			{ 'bSortable': false, 'aTargets': [ 0 ] }
+		"aoColumnDefs": [
+			{ "asSorting": [ "asc" ], "aTargets": [ 0 ] }
 		]
 	});
 
