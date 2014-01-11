4b8e9c8b895ececde92f412c74182607cd3734a9
ThemeBoy <themeboy@themeboy.com>
2014-01-12 02:48:45 +1100

    Link to SportsPress themes from plugins page

--- admin/hooks/plugin-action-links.php
+++ admin/hooks/plugin-action-links.php
@@ -1,7 +1,10 @@
 <?php
 function sportspress_plugin_action_links( $links ) { 
   $settings_link = '<a href="options-general.php?page=sportspress">' . __( 'Settings', 'sportspress' ) . '</a>';
-  array_unshift( $links, $settings_link );
+  $themes_link = '<a href="http://themeboy.com/themes/?ref=sportspress" target="_blank">' . __( 'Themes', 'sportspress' ) . '</a>';
+
+  array_unshift( $links, $settings_link, $themes_link );
+
   return $links;
 }
  
