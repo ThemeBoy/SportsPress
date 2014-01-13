14d42a2baa16e2c7c210c806d8551c3c3687f08f
ThemeBoy <themeboy@themeboy.com>
2014-01-14 04:43:18 +1100

    Add fit image size for team logos

--- admin/hooks/plugins-loaded.php
+++ admin/hooks/plugins-loaded.php
@@ -4,8 +4,10 @@ function sportspress_plugins_loaded() {
     // Load plugin textdomain
 	if ( function_exists( 'load_plugin_textdomain' ) ) {
 
+		// SportsPress
     	load_plugin_textdomain ( 'sportspress', false, dirname( SPORTSPRESS_PLUGIN_BASENAME ) . '/languages/' );
 
+    	// Countries
     	load_plugin_textdomain ( 'countries', false, dirname( SPORTSPRESS_PLUGIN_BASENAME ) . '/languages/' );
 
     }
@@ -13,22 +15,26 @@ function sportspress_plugins_loaded() {
     // Add image sizes
 	if ( function_exists( 'add_image_size' ) ) {
 
+		// Header (1680 width minus 15 for scrollbar)
+		add_image_size( 'sportspress-header', 1665, 705, true );
+
 		// Standard (4:3)
 		add_image_size( 'sportspress-standard', 800, 600, true );
 		add_image_size( 'sportspress-standard-thumbnail', 400, 300, true );
 
 		// Wide (16:9)
-		add_image_size( 'sportspress-wide', 640, 360, true );
+		add_image_size( 'sportspress-wide', 800, 450, true );
 		add_image_size( 'sportspress-wide-thumbnail', 400, 225, true );
 
 		// Square (1:1)
 		add_image_size( 'sportspress-square', 612, 612, true );
 		add_image_size( 'sportspress-square-thumbnail', 200, 200, true );
 
-		// Header (1680 width minus 15 for scrollbar)
-		add_image_size( 'sportspress-header', 1665, 705, true );
+		// Fit (Proportional)
+		add_image_size( 'sportspress-fit',  300, 300, false );
+		add_image_size( 'sportspress-fit-thumbnail',  150, 150, false );
 
-		// Icon (32px uncropped)
+		// Icon (Proportional)
 		add_image_size( 'sportspress-icon',  32, 32, false );
 
 	}
