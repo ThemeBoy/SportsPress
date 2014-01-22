1a4319a6419cee9e708a6b06636f79f8b22afbe5
Brian Miyaji <brian@themeboy.com>
2014-01-22 21:03:12 +1100

    Same data tables behaviour for every table

--- assets/js/sportspress.js
+++ assets/js/sportspress.js
@@ -1,42 +1,7 @@
 (function($) {
 
-	// League Table Sorting
-	$(".sp-league-table").dataTable({
-		"aaSorting": [],
-		"bAutoWidth": false,
-		"bFilter": false,
-		"bInfo": false,
-		"bPaginate": false,
-		"bSort": true,
-	    "oLanguage": {
-	      "oAria": {
-	        "sSortAscending": "",
-	        "sSortDescending": ""
-	      }
-	    }
-	});
-
-	// Player List Sorting
-	$(".sp-player-list").dataTable({
-		"aaSorting": [],
-		"bAutoWidth": false,
-		"bFilter": false,
-		"bInfo": false,
-		"bPaginate": false,
-		"bSort": true,
-	    "oLanguage": {
-	      "oAria": {
-	        "sSortAscending": "",
-	        "sSortDescending": ""
-	      }
-	    },
-		"aoColumnDefs": [
-			{ "asSorting": [ "asc" ], "aTargets": [ 0 ] },
-		]
-	});
-
-	// Player Statistics Sorting
-	$(".sp-player-statistics").dataTable({
+	// Data Tables
+	$(".sp-data-table").dataTable({
 		"aaSorting": [],
 		"bAutoWidth": false,
 		"bFilter": false,
