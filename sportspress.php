bfdfdc4ae899b086c2282dc2a712829987011d7e
ThemeBoy <themeboy@themeboy.com>
2014-01-24 03:22:48 +1100

    Add site admin capabilities for multisite

--- sportspress.php
+++ sportspress.php
@@ -78,6 +78,7 @@ require_once dirname( __FILE__ ) . '/admin/hooks/after-setup-theme.php';
 require_once dirname( __FILE__ ) . '/admin/hooks/wp-enqueue-scripts.php';
 
 // Admin request actions
+require_once dirname( __FILE__ ) . '/admin/hooks/admin-init.php';
 require_once dirname( __FILE__ ) . '/admin/hooks/admin-menu.php';
 require_once dirname( __FILE__ ) . '/admin/hooks/admin-enqueue-scripts.php';
 require_once dirname( __FILE__ ) . '/admin/hooks/admin-print-styles.php';
