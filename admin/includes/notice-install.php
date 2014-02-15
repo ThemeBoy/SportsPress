ad9c576d4ab47dd21e0628cdb5d1510aaa36a5d9
ThemeBoy <themeboy@themeboy.com>
2014-02-15 23:46:50 +1100

    Style install notice

--- admin/includes/notice-install.php
+++ admin/includes/notice-install.php
@@ -2,9 +2,9 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 <div id="message" class="updated sportspress-message">
-	<h3><?php _e( '<strong>Welcome to SportsPress</strong> &#8211; Get Started', 'sportspress' ); ?></h3>
+	<p><?php _e( '<strong>Welcome to SportsPress</strong> &#8211; Get Started', 'sportspress' ); ?></p>
 	<p class="submit">
-		<a class="button button-primary button-hero" href="<?php echo admin_url('options-general.php?page=sportspress'); ?>"><?php _e( 'Go to SportsPress Settings', 'sportspress' ); ?></a>
-		<a class="button button-secondary button-hero" href="<?php echo add_query_arg('sportspress_installed', '1' ); ?>"><?php _e( 'Skip setup', 'sportspress' ); ?></a>
+		<a class="button button-primary" href="<?php echo admin_url('options-general.php?page=sportspress'); ?>"><?php _e( 'Go to SportsPress Settings', 'sportspress' ); ?></a>
+		<a class="button button-secondary" href="<?php echo add_query_arg('sportspress_installed', '1' ); ?>"><?php _e( 'Skip setup', 'sportspress' ); ?></a>
 	</p>
 </div>
