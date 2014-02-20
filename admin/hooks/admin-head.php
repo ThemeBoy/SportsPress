2f5760a9ec5abc5c8167d5e96a2d1c80f2f24664
ThemeBoy <themeboy@themeboy.com>
2014-02-20 15:45:41 +1100

    Highlight admin menu when adding new config

--- admin/hooks/admin-head.php
+++ admin/hooks/admin-head.php
@@ -1,10 +1,9 @@
 <?php
 function sportspress_admin_head() {
 	global $typenow;
-
-	if ( in_array( $typenow, array( 'sp_result', 'sp_outcome', 'sp_column', 'sp_statistic', 'sp_metric' ) ) ):
+	if ( in_array( $typenow, array( 'sp_result', 'sp_outcome', 'sp_column', 'sp_statistic', 'sp_metric' ) ) )
 		sportspress_highlight_admin_menu();
-	endif;
 }
 add_action( 'admin_head-edit.php', 'sportspress_admin_head', 10, 2 );
 add_action( 'admin_head-post.php', 'sportspress_admin_head', 10, 2 );
+add_action( 'admin_head-post-new.php', 'sportspress_admin_head', 10, 2 );
