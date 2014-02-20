ef1f919aa883f477a48f5733d8816ac72fdecf2f
Brian Miyaji <brian@themeboy.com>
2014-02-20 14:44:24 +1100

    Update Event meta box names and menu label

--- admin/post-types/event.php
+++ admin/post-types/event.php
@@ -4,7 +4,7 @@ function sportspress_event_post_init() {
 	$singular_name = __( 'Event', 'sportspress' );
 	$lowercase_name = __( 'events', 'sportspress' );
 	$labels = sportspress_get_post_labels( $name, $singular_name, $lowercase_name );
-	//$labels['menu_name'] = __( 'SportsPress', 'sportspress' );
+	$labels['menu_name'] = __( 'Schedule', 'sportspress' );
 	$args = array(
 		'label' => $name,
 		'labels' => $labels,
@@ -32,19 +32,23 @@ add_filter( 'the_posts', 'sportspress_event_display_scheduled' );
 
 function sportspress_event_meta_init( $post ) {
 	$teams = (array)get_post_meta( $post->ID, 'sp_team', false );
+	$players = (array)get_post_meta( $post->ID, 'sp_player', false );
 
 	remove_meta_box( 'submitdiv', 'sp_event', 'side' );
 	remove_meta_box( 'sp_venuediv', 'sp_event', 'side' );
 	remove_meta_box( 'sp_leaguediv', 'sp_event', 'side' );
 	remove_meta_box( 'sp_seasondiv', 'sp_event', 'side' );
-	
+
 	add_meta_box( 'submitdiv', __( 'Event', 'sportspress' ), 'post_submit_meta_box', 'sp_event', 'side', 'high' );
 	add_meta_box( 'sp_detailsdiv', __( 'Details', 'sportspress' ), 'sportspress_event_details_meta', 'sp_event', 'side', 'high' );
 	add_meta_box( 'sp_teamdiv', __( 'Teams', 'sportspress' ), 'sportspress_event_team_meta', 'sp_event', 'side', 'high' );
-	if ( sizeof( $teams ) > 0 ):
+	if ( sizeof( $teams ) > 0 )
 		add_meta_box( 'sp_resultsdiv', __( 'Results', 'sportspress' ), 'sportspress_event_results_meta', 'sp_event', 'normal', 'high' );
-		add_meta_box( 'sp_playersdiv', __( 'Players', 'sportspress' ), 'sportspress_event_players_meta', 'sp_event', 'normal', 'high' );
-	endif;
+
+	do_action( 'sportspress_event_meta_init' );
+
+	if ( sizeof( $players ) > 0 )
+		add_meta_box( 'sp_statisticsdiv', __( 'Statistics', 'sportspress' ), 'sportspress_event_statistics_meta', 'sp_event', 'normal', 'high' );
 	add_meta_box( 'sp_articlediv', __( 'Article', 'sportspress' ), 'sportspress_event_article_meta', 'sp_event', 'normal', 'high' );
 }
 
@@ -149,11 +153,11 @@ function sportspress_event_team_meta( $post ) {
 	sportspress_nonce();
 }
 
-function sportspress_event_players_meta( $post ) {
+function sportspress_event_statistics_meta( $post ) {
 	$teams = (array)get_post_meta( $post->ID, 'sp_team', false );
 	$stats = (array)get_post_meta( $post->ID, 'sp_players', true );
 
-	// Get columns from result variables
+	// Get columns from statistic variables
 	$columns = sportspress_get_var_labels( 'sp_statistic' );
 
 	foreach ( $teams as $key => $team_id ):
@@ -171,7 +175,6 @@ function sportspress_event_players_meta( $post ) {
 		<?php
 
 	endforeach;
-
 }
 
 function sportspress_event_results_meta( $post ) {
