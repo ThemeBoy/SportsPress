7f4615bdb86e04933b6c83decf919f4813abf201
ThemeBoy <support@themeboy.com>
2014-03-14 12:37:31 +1100

    Enable admin post list sorting

--- admin/hooks/pre-get-posts.php
+++ admin/hooks/pre-get-posts.php
@@ -2,6 +2,9 @@
 function sportspress_pre_get_posts( $query ) {
 
 	if ( is_admin() ):
+		if ( isset( $query->query[ 'orderby' ] ) || isset( $query->query[ 'order' ] ) ):
+			return $query;
+		endif;
 		$post_type = $query->query['post_type'];
 
 		if ( in_array( $post_type, array( 'sp_result', 'sp_outcome', 'sp_column', 'sp_statistic' ) ) ):
