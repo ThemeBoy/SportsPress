c284590f93e50de15a7fde67529b47656a0b8222
Brian Miyaji <brian@themeboy.com>
2014-04-02 09:46:37 +1100

    Display default shortcode format when format not set

--- includes/admin/post-types/meta-boxes/class-sp-meta-box-list-shortcode.php
+++ includes/admin/post-types/meta-boxes/class-sp-meta-box-list-shortcode.php
@@ -5,7 +5,7 @@
  * @author 		ThemeBoy
  * @category 	Admin
  * @package 	SportsPress/Admin/Meta Boxes
- * @version     0.7
+ * @version     0.7.4
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -20,6 +20,7 @@ class SP_Meta_Box_List_Shortcode {
 	 */
 	public static function output( $post ) {
 		$the_format = get_post_meta( $post->ID, 'sp_format', true );
+		if ( ! $the_format ) $the_format = 'list';
 		?>
 		<p class="howto">
 			<?php _e( 'Copy this code and paste it into your post, page or text widget content.', 'sportspress' ); ?>
