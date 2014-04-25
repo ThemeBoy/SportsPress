0406de755221c765c9a3293b84b2071af5f828e2
Brian Miyaji <brian@themeboy.com>
2014-04-25 18:16:13 +1000

    Create data formatter

--- includes/admin/post-types/meta-boxes/class-sp-meta-box-list-data.php
+++ includes/admin/post-types/meta-boxes/class-sp-meta-box-list-data.php
@@ -20,8 +20,8 @@ class SP_Meta_Box_List_Data {
 	 */
 	public static function output( $post ) {
 		list( $columns, $usecolumns, $data, $placeholders, $merged ) = sp_get_player_list_data( $post->ID, true );
-
-		sp_edit_player_list_table( $columns, $usecolumns, $data, $placeholders );
+		$adjustments = get_post_meta( $post->ID, 'sp_adjustments', true );
+		self::table( $columns, $usecolumns, $data, $placeholders, $adjustments );
 	}
 
 	/**
@@ -29,6 +29,68 @@ class SP_Meta_Box_List_Data {
 	 */
 	public static function save( $post_id, $post ) {
 		update_post_meta( $post_id, 'sp_columns', sp_array_value( $_POST, 'sp_columns', array() ) );
+		update_post_meta( $post_id, 'sp_adjustments', sp_array_value( $_POST, 'sp_adjustments', array() ) );
 		update_post_meta( $post_id, 'sp_players', sp_array_value( $_POST, 'sp_players', array() ) );
+	}
+
+	/**
+	 * Admin edit table
+	 */
+	public static function table( $columns = array(), $usecolumns = null, $data = array(), $placeholders = array() ) {
+		if ( is_array( $usecolumns ) )
+			$usecolumns = array_filter( $usecolumns );
+		?>
+		<div class="sp-data-table-container">
+			<table class="widefat sp-data-table sp-player-list-table">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th><?php _e( 'Player', 'sportspress' ); ?></th>
+						<?php foreach ( $columns as $key => $label ): ?>
+							<th><label for="sp_columns_<?php echo $key; ?>">
+								<input type="checkbox" name="sp_columns[]" value="<?php echo $key; ?>" id="sp_columns_<?php echo $key; ?>" <?php checked( ! is_array( $usecolumns ) || in_array( $key, $usecolumns ) ); ?>>
+								<?php echo $label; ?>
+							</label></th>
+						<?php endforeach; ?>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					if ( is_array( $data ) && sizeof( $data ) > 0 ):
+						$i = 0;
+						foreach ( $data as $player_id => $player_stats ):
+							if ( !$player_id ) continue;
+							$div = get_term( $player_id, 'sp_season' );
+							$number = get_post_meta( $player_id, 'sp_number', true );
+							?>
+							<tr class="sp-row sp-post<?php if ( $i % 2 == 0 ) echo ' alternate'; ?>">
+								<td><?php echo ( $number ? $number : '&nbsp;' ); ?></td>
+								<td>
+									<?php echo get_the_title( $player_id ); ?>
+								</td>
+								<?php foreach( $columns as $column => $label ):
+									$value = sp_array_value( $player_stats, $column, '' );
+									$placeholder = sp_array_value( sp_array_value( $placeholders, $player_id, array() ), $column, 0 );
+									?>
+									<td><input type="text" name="sp_players[<?php echo $player_id; ?>][<?php echo $column; ?>]" value="<?php echo $value; ?>" placeholder="<?php echo $placeholder; ?>" /></td>
+								<?php endforeach; ?>
+							</tr>
+							<?php
+							$i++;
+						endforeach;
+					else:
+					?>
+					<tr class="sp-row alternate">
+						<td colspan="<?php $colspan = sizeof( $columns ) + 1; echo $colspan; ?>">
+							<?php printf( __( 'Select %s', 'sportspress' ), __( 'Players', 'sportspress' ) ); ?>
+						</td>
+					</tr>
+					<?php
+					endif;
+					?>
+				</tbody>
+			</table>
+		</div>
+		<?php
 	}
 }
