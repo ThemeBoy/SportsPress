58788d6ce0671fe023e199557d82ae41354a71dd
ThemeBoy <support@themeboy.com>
2014-05-01 16:30:36 +1000

    Remove unused JS

--- assets/js/admin/sportspress-admin.js
+++ assets/js/admin/sportspress-admin.js
@@ -1,14 +1,5 @@
 jQuery(document).ready(function($){
 
-	// Display custom sport name field as needed
-	$("body.toplevel_page_sportspress #sportspress_sport").change(function() {
-		$target = $("#sportspress_custom_sport_name");
-		if ( $(this).val() == "custom" )
-			$target.show();
-		else
-			$target.hide();
-	});
-
 	// Tiptip
 	$(".tips").tipTip({
 		delay: 200,
