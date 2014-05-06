fd1b45803ced7b62aadf1e6fb15bc25beb5957c6
ThemeBoy <support@themeboy.com>
2014-05-06 12:25:01 +1000

    Fix filter players by team closes #23

--- includes/admin/post-types/class-sp-admin-cpt-player.php
+++ includes/admin/post-types/class-sp-admin-cpt-player.php
@@ -5,7 +5,7 @@
  * @author 		ThemeBoy
  * @category 	Admin
  * @package 	SportsPress/Admin/Post Types
- * @version     0.7
+ * @version     0.9
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -183,7 +183,7 @@ class SP_Admin_CPT_Player extends SP_Admin_CPT {
 
 	    if ( $typenow == 'sp_player' ) {
 
-	    	if ( isset( $_GET['team'] ) ) {
+	    	if ( ! empty( $_GET['team'] ) ) {
 		    	$query->query_vars['meta_value'] 	= $_GET['team'];
 		        $query->query_vars['meta_key'] 		= 'sp_team';
 		    }
