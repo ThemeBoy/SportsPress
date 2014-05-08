ba579cfb9ea2ab7f91c8ee156b6141150d25bbce
Brian Miyaji <brian@themeboy.com>
2014-05-08 13:52:25 +1000

    Fix event team limit issue, update to 0.8.6

--- includes/admin/settings/class-sp-settings-events.php
+++ includes/admin/settings/class-sp-settings-events.php
@@ -5,7 +5,7 @@
  * @author 		ThemeBoy
  * @category 	Admin
  * @package 	SportsPress/Admin
- * @version     0.8
+ * @version     0.8.6
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
