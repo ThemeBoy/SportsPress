07fe53e0dad7b00050ad20e57e0ebd283331398d
Brian Miyaji <brian@themeboy.com>
2014-05-12 22:33:12 +1000

    Add staff importer

--- includes/admin/class-sp-admin-importers.php
+++ includes/admin/class-sp-admin-importers.php
@@ -31,22 +31,15 @@ class SP_Admin_Importers {
 		register_importer( 'sportspress_event_csv', __( 'SportsPress Events (CSV)', 'sportspress' ), __( 'Import <strong>events</strong> from a csv file.', 'sportspress'), array( $this, 'events_importer' ) );
 		register_importer( 'sportspress_team_csv', __( 'SportsPress Teams (CSV)', 'sportspress' ), __( 'Import <strong>teams</strong> from a csv file.', 'sportspress'), array( $this, 'teams_importer' ) );
 		register_importer( 'sportspress_player_csv', __( 'SportsPress Players (CSV)', 'sportspress' ), __( 'Import <strong>players</strong> from a csv file.', 'sportspress'), array( $this, 'players_importer' ) );
+		register_importer( 'sportspress_staff_csv', __( 'SportsPress Staff (CSV)', 'sportspress' ), __( 'Import <strong>staff</strong> from a csv file.', 'sportspress'), array( $this, 'staff_importer' ) );
 	}
 
 	/**
 	 * Add menu item
 	 */
 	public function events_importer() {
-		// Load Importer API
-	    require_once ABSPATH . 'wp-admin/includes/import.php';
-
-	    if ( ! class_exists( 'WP_Importer' ) ) {
-	        $class_wp_importer = ABSPATH . 'wp-admin/includes/class-wp-importer.php';
-	        if ( file_exists( $class_wp_importer ) )
-	            require $class_wp_importer;
-	    }
-
-	    // Includes
+		$this->includes();
+		
 	    require 'importers/class-sp-event-importer.php';
 
 	    // Dispatch
@@ -58,16 +51,8 @@ class SP_Admin_Importers {
 	 * Add menu item
 	 */
 	public function teams_importer() {
-		// Load Importer API
-	    require_once ABSPATH . 'wp-admin/includes/import.php';
-
-	    if ( ! class_exists( 'WP_Importer' ) ) {
-	        $class_wp_importer = ABSPATH . 'wp-admin/includes/class-wp-importer.php';
-	        if ( file_exists( $class_wp_importer ) )
-	            require $class_wp_importer;
-	    }
-
-	    // Includes
+		$this->includes();
+		
 	    require 'importers/class-sp-team-importer.php';
 
 	    // Dispatch
@@ -79,6 +64,29 @@ class SP_Admin_Importers {
 	 * Add menu item
 	 */
 	public function players_importer() {
+		$this->includes();
+		
+	    require 'importers/class-sp-player-importer.php';
+
+	    // Dispatch
+	    $importer = new SP_Player_Importer();
+	    $importer->dispatch();
+	}
+
+	/**
+	 * Add menu item
+	 */
+	public function staff_importer() {
+		$this->includes();
+
+	    require 'importers/class-sp-staff-importer.php';
+
+	    // Dispatch
+	    $importer = new SP_Staff_Importer();
+	    $importer->dispatch();
+	}
+
+	public static function includes() {
 		// Load Importer API
 	    require_once ABSPATH . 'wp-admin/includes/import.php';
 
@@ -88,12 +96,7 @@ class SP_Admin_Importers {
 	            require $class_wp_importer;
 	    }
 
-	    // Includes
-	    require 'importers/class-sp-player-importer.php';
-
-	    // Dispatch
-	    $importer = new SP_Player_Importer();
-	    $importer->dispatch();
+	    require 'importers/class-sp-importer.php';
 	}
 }
 
--- includes/admin/importers/class-sp-player-importer.php
+++ includes/admin/importers/class-sp-player-importer.php
@@ -5,21 +5,13 @@
  * @author 		ThemeBoy
  * @category 	Admin
  * @package 	SportsPress/Admin/Importers
- * @version     0.2.11
+ * @version     0.9
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 if ( class_exists( 'WP_Importer' ) ) {
-	class SP_Player_Importer extends WP_Importer {
-
-		var $id;
-		var $file_url;
-		var $import_page;
-		var $delimiter;
-		var $posts = array();
-		var $imported;
-		var $skipped;
+	class SP_Player_Importer extends SP_Importer {
 
 		/**
 		 * __construct function.
@@ -29,62 +21,6 @@ if ( class_exists( 'WP_Importer' ) ) {
 		 */
 		public function __construct() {
 			$this->import_page = 'sportspress_player_csv';
-		}
-
-		/**
-		 * Registered callback function for the WordPress Importer
-		 *
-		 * Manages the three separate stages of the CSV import process
-		 */
-		function dispatch() {
-			$this->header();
-
-			if ( ! empty( $_POST['delimiter'] ) )
-				$this->delimiter = stripslashes( trim( $_POST['delimiter'] ) );
-
-			if ( ! $this->delimiter )
-				$this->delimiter = ',';
-
-			$step = empty( $_GET['step'] ) ? 0 : (int) $_GET['step'];
-			switch ( $step ):
-				case 0:
-					$this->greet();
-					break;
-				case 1:
-					check_admin_referer( 'import-upload' );
-					if ( $this->handle_upload() ):
-
-						if ( $this->id )
-							$file = get_attached_file( $this->id );
-						else
-							$file = ABSPATH . $this->file_url;
-
-						add_filter( 'http_request_timeout', array( $this, 'bump_request_timeout' ) );
-
-						if ( function_exists( 'gc_enable' ) )
-							gc_enable();
-
-						@set_time_limit(0);
-						@ob_flush();
-						@flush();
-
-						$this->import( $file );
-					endif;
-					break;
-			endswitch;
-			$this->footer();
-		}
-
-		/**
-		 * format_data_from_csv function.
-		 *
-		 * @access public
-		 * @param mixed $data
-		 * @param string $enc
-		 * @return string
-		 */
-		function format_data_from_csv( $data, $enc ) {
-			return ( $enc == 'UTF-8' ) ? $data : utf8_encode( $data );
 		}
 
 		/**
@@ -217,44 +153,6 @@ if ( class_exists( 'WP_Importer' ) ) {
 		}
 
 		/**
-		 * Handles the CSV upload and initial parsing of the file to prepare for
-		 * displaying author import options
-		 *
-		 * @return bool False if error uploading or invalid file, true otherwise
-		 */
-		function handle_upload() {
-
-			if ( empty( $_POST['file_url'] ) ) {
-
-				$file = wp_import_handle_upload();
-
-				if ( isset( $file['error'] ) ) {
-					echo '<p><strong>' . __( 'Sorry, there has been an error.', 'sportspress' ) . '</strong><br />';
-					echo esc_html( $file['error'] ) . '</p>';
-					return false;
-				}
-
-				$this->id = (int) $file['id'];
-
-			} else {
-
-				if ( file_exists( ABSPATH . $_POST['file_url'] ) ) {
-
-					$this->file_url = esc_attr( $_POST['file_url'] );
-
-				} else {
-
-					echo '<p><strong>' . __( 'Sorry, there has been an error.', 'sportspress' ) . '</strong></p>';
-					return false;
-
-				}
-
-			}
-
-			return true;
-		}
-
-		/**
 		 * header function.
 		 *
 		 * @access public
@@ -262,16 +160,6 @@ if ( class_exists( 'WP_Importer' ) ) {
 		 */
 		function header() {
 			echo '<div class="wrap"><h2>' . __( 'Import Players', 'sportspress' ) . '</h2>';
-		}
-
-		/**
-		 * footer function.
-		 *
-		 * @access public
-		 * @return void
-		 */
-		function footer() {
-			echo '</div>';
 		}
 
 		/**
@@ -333,15 +221,6 @@ if ( class_exists( 'WP_Importer' ) ) {
 			endif;
 
 			echo '</div>';
-		}
-
-		/**
-		 * Added to http_request_timeout filter to force timeout at 60 seconds during import
-		 * @param  int $val
-		 * @return int 60
-		 */
-		function bump_request_timeout( $val ) {
-			return 60;
 		}
 	}
 }
