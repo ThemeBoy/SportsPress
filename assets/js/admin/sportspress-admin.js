335b582f997600ea0597822179fb7b34e65b9661
ThemeBoy <support@themeboy.com>
2014-06-18 13:25:18 +1000

    Fix select all checkbox behavior

--- assets/js/admin/sportspress-admin.js
+++ assets/js/admin/sportspress-admin.js
@@ -32,7 +32,7 @@ jQuery(document).ready(function($){
 
 	// Tab switcher
 	$(".sp-tab-panel").siblings(".sp-tab-bar").find("a").click(function() {
-		$(this).closest("li").removeClass("wp-tab").addClass("wp-tab-active").siblings().removeClass("wp-tab-active").addClass("wp-tab").closest(".wp-tab-bar").siblings($(this).attr("href")).show().siblings(".wp-tab-panel").hide();
+		$(this).closest("li").removeClass("wp-tab").addClass("wp-tab-active").siblings().removeClass("wp-tab-active").addClass("wp-tab").closest(".wp-tab-bar").siblings($(this).attr("href")).show().trigger('checkCheck').siblings(".wp-tab-panel").hide();
 		return false;
 	});
 
