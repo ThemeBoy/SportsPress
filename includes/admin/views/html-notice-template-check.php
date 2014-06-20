98489ac0cb9cc321f742f3607f2f693bdefaaf3d
Brian Miyaji <brian@themeboy.com>
2014-06-21 01:04:24 +1000

    Add missing admin notice

---
+++ includes/admin/views/html-notice-template-check.php
@@ -0,0 +1,7 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+?>
+<div id="message" class="updated sportspress-message wc-connect">
+	<p><?php _e( '<strong>Your theme has bundled outdated copies of SportsPress template files</strong> &#8211; if you encounter functionality issues on the frontend this could the reason. Ensure you update or remove them (in general we recommend only bundling the template files you actually need to customize). See the system report for full details.', 'sportspress' ); ?></p>
+	<p class="submit"><a class="button-primary" href="<?php echo esc_url( admin_url( 'admin.php?page=sp-status' ) ); ?>"><?php _e( 'System Status', 'sportspress' ); ?></a> <a class="skip button-primary" href="<?php echo esc_url( add_query_arg( 'hide_template_files_notice', 'true' ) ); ?>"><?php _e( 'Hide this notice', 'sportspress' ); ?></a></p>
+</div>
