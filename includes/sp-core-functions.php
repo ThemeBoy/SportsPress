eef43be7e9aeee20758cce79a88b61af957de247
Brian Miyaji <brian@themeboy.com>
2014-06-27 00:34:45 +1000

    Tag version 1.1.4

--- includes/sp-core-functions.php
+++ includes/sp-core-functions.php
@@ -7,7 +7,7 @@
  * @author 		ThemeBoy
  * @category 	Core
  * @package 	SportsPress/Functions
- * @version     1.1
+ * @version     1.1.4
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
