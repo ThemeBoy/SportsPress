fd3f867fb7f9d481c0b632bd9c2f2a8c34096ee9
ThemeBoy <support@themeboy.com>
2014-07-20 00:33:43 +1000

    Fix hide theme support notice

--- includes/admin/views/html-notice-theme-support.php
+++ includes/admin/views/html-notice-theme-support.php
@@ -5,6 +5,6 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 	<p><?php _e( '<strong>Your theme does not declare SportsPress support</strong> &#8211; if you encounter layout issues please read our integration guide or choose a SportsPress theme :)', 'sportspress' ); ?></p>
 	<p class="submit">
 		<a class="button-primary" href="http://sportspresspro.com/docs/theme-integration-guide/"><?php _e( 'Theme Integration Guide', 'sportspress' ); ?></a>
-		<a class="button-secondary" href="<?php echo add_query_arg( 'hide_sportspress_theme_support_check', 'true' ); ?>"><?php _e( 'Hide this notice', 'sportspress' ); ?></a>
+		<a class="button-secondary" href="<?php echo add_query_arg( 'hide_theme_support_notice', 'true' ); ?>"><?php _e( 'Hide this notice', 'sportspress' ); ?></a>
 	</p>
 </div>
