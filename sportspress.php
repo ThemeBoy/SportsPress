4a9366c84921114b1eb6c563f1616bc3afa9d21b
ThemeBoy <support@themeboy.com>
2014-07-20 01:17:43 +1000

    Tag version 1.2.2

--- sportspress.php
+++ sportspress.php
@@ -3,7 +3,7 @@
  * Plugin Name: SportsPress
  * Plugin URI: http://themeboy.com/sportspress/
  * Description: Manage your club and its players, staff, events, league tables, and player lists.
- * Version: 1.2.1
+ * Version: 1.2.2
  * Author: ThemeBoy
  * Author URI: http://themeboy.com/
  * Requires at least: 3.8
@@ -26,14 +26,14 @@ if ( ! class_exists( 'SportsPress' ) ) :
  * Main SportsPress Class
  *
  * @class SportsPress
- * @version	1.2.1
+ * @version	1.2.2
  */
 final class SportsPress {
 
 	/**
 	 * @var string
 	 */
-	public $version = '1.2.1';
+	public $version = '1.2.2';
 
 	/**
 	 * @var SporsPress The single instance of the class
