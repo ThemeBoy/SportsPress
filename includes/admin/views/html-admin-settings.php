94fd67014ca308acf9dd368cfc9cd4b8876e90ad
Brian Miyaji <brian@themeboy.com>
2014-08-25 00:22:59 +1000

    Display link in settings footer

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -4,19 +4,22 @@
 			<?php foreach ( $tabs as $name => $label ): ?><a href="<?php echo admin_url( 'admin.php?page=sportspress&tab=' . $name ); ?>" class="nav-tab <?php echo ( $current_tab == $name ? 'nav-tab-active' : '' ); ?>"><?php echo $label; ?></a><?php endforeach; ?>
 			<?php do_action( 'sportspress_settings_tabs' ); ?>
 		</h2>
-
 		<?php
 			do_action( 'sportspress_sections_' . $current_tab );
 			do_action( 'sportspress_settings_' . $current_tab );
 			do_action( 'sportspress_settings_tabs_' . $current_tab ); // @deprecated hook
 		?>
-
-        <p class="submit">
-        	<?php if ( ! isset( $GLOBALS['hide_save_button'] ) ) : ?>
-        		<input name="save" class="button-primary" type="submit" value="<?php _e( 'Save changes', 'sportspress' ); ?>" />
-        	<?php endif; ?>
-        	<input type="hidden" name="subtab" id="last_tab" />
-        	<?php wp_nonce_field( 'sportspress-settings' ); ?>
-        </p>
+	    <p class="submit">
+	    	<?php if ( ! isset( $GLOBALS['hide_save_button'] ) ) : ?>
+	    		<input name="save" class="button-primary" type="submit" value="<?php _e( 'Save changes', 'sportspress' ); ?>" />
+	    	<?php endif; ?>
+	    	<input type="hidden" name="subtab" id="last_tab" />
+	    	<?php wp_nonce_field( 'sportspress-settings' ); ?>
+	    </p>
 	</form>
+	<p>
+		<a href="http://wordpress.org/support/view/plugin-reviews/sportspress?rate=5#postform">
+			<?php _e( 'Love SportsPress? Help spread the word by rating us 5★ on WordPress.org', 'sportspress' ); ?>
+		</a>
+	</p>
 </div>
