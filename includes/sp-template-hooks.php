054ae9750ca7eb6859b6ca0ade909b214d4794e9
Brian Miyaji <brian@themeboy.com>
2014-08-26 03:39:01 +1000

    Update template hooks version

--- includes/sp-template-hooks.php
+++ includes/sp-template-hooks.php
@@ -7,7 +7,7 @@
  * @author 		ThemeBoy
  * @category 	Core
  * @package 	SportsPress/Functions
- * @version     1.1.4
+ * @version     1.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
