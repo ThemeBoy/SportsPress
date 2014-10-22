77cce195702bba1518eb65679cbd129b5d642f00
Brian Miyaji <brian@themeboy.com>
2014-10-22 18:52:05 +1100

    Add player photo template

---
+++ templates/player-photo.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Player Photo
+ *
+ * @author 		ThemeBoy
+ * @package 	SportsPress/Templates
+ * @version     1.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( get_option( 'sportspress_player_show_photo', 'yes' ) === 'no' ) return;
+
+if ( ! isset( $id ) )
+	$id = get_the_ID();
+
+if ( has_post_thumbnail( $id ) ):
+	?>
+	<div class="sp-template sp-template-player-photo sp-template-photo sp-player-photo">
+		<?php echo get_the_post_thumbnail( $id, 'medium' ); ?>
+	</div>
+	<?php
+endif;
