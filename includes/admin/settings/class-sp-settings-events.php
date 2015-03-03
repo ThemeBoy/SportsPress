a06635e031411b8c1a13e4f4c278f264e4fed8dd
Brian Miyaji <brian@themeboy.com>
2015-03-03 23:10:29 +1100

    Add ability to split players by team

--- includes/admin/settings/class-sp-settings-events.php
+++ includes/admin/settings/class-sp-settings-events.php
@@ -234,9 +234,17 @@ class SP_Settings_Events extends SP_Settings_Page {
 				),
 
 				array(
+					'desc' 		=> __( 'Split players by team', 'sportspress' ),
+					'id' 		=> 'sportspress_event_split_players_by_team',
+					'default'	=> 'yes',
+					'type' 		=> 'checkbox',
+					'checkboxgroup'		=> '',
+				),
+
+				array(
 					'desc' 		=> __( 'Split players by position', 'sportspress' ),
 					'id' 		=> 'sportspress_event_split_players_by_position',
-					'default'	=> 'false',
+					'default'	=> 'no',
 					'type' 		=> 'checkbox',
 					'checkboxgroup'		=> 'end',
 				),
