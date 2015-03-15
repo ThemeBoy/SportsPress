402544d4fb03adf5b7d85ed995a4ad1c12cf650d
Brian Miyaji <brian@themeboy.com>
2015-03-15 23:01:04 +1100

    Automatically delete source language file

--- mo.sh
+++ mo.sh
@@ -1,7 +1,9 @@
 for file in $(find ./includes/sportspress/languages/ -name *.po -type f);
 	do msgfmt "$file" -o "${file%po}mo"; rm "$file";
 done
+rm "includes/sportspress/languages/sportspress-en_US.mo";
 
 for file in $(find ./languages/ -name *.po -type f);
 	do msgfmt "$file" -o "${file%po}mo"; rm "$file";
 done
+rm "languages/sportspress-pro-en_US.mo";
