99ffe44af5668afd6c16cfe1f6a222509ae3cccd
Brian Miyaji <brian@themeboy.com>
2015-05-16 03:01:22 +1000

    Fix updater URL

--- includes/sportspress-updater/sportspress-updater.php
+++ includes/sportspress-updater/sportspress-updater.php
@@ -56,7 +56,7 @@ class SportsPress_Updater {
 		$license_key = get_option( 'sportspress_pro_license_key' );
 		if ( $license_key ) {
 			require_once( 'includes/class-sp-updater.php' );
-			new SP_Updater( 'http://wp-updates.com/api/2/plugin', plugin_basename( SP_PLUGIN_FILE ), $license_key );
+			new SP_Updater( 'http://wp-updates.com/api/2/plugin', plugin_basename( SP_PRO_PLUGIN_FILE ), $license_key );
 		}
 	}
 
