fc2fca05097ff5dd9a4e7a24f963e6164f484256
Brian Miyaji <brian@themeboy.com>
2015-07-16 18:10:31 +1000

    Fix duplicate league table headings as per https://wordpress.org/support/topic/league-table-name-repeated

--- templates/team-tables.php
+++ templates/team-tables.php
@@ -18,11 +18,5 @@ $tables = $team->tables();
 foreach ( $tables as $table ):
 	if ( ! $table ) continue;
 
-	if ( sizeof( $tables ) > 1 ):
-		?>
-		<h4 class="sp-table-caption"><?php echo $table->post_title; ?></h4>
-		<?php
-	endif;
-
 	sp_get_template( 'league-table.php', array( 'id' => $table->ID, 'highlight' => $id ) );
 endforeach;
