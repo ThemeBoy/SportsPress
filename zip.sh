351b799dcdec1385fa44778daf981c7a6159f76b
Brian Miyaji <brian@themeboy.com>
2016-01-20 21:28:02 +1100

    Zip the entire folder

--- zip.sh
+++ zip.sh
@@ -1,6 +1,4 @@
 echo "Enter version number."
 read version
 
-mkdir ../../archives/sportspress-pro/$version
-
-zip -r ../../archives/sportspress-pro/$version/sportspress-pro.zip . -x "*.DS_Store" ".tx/*" "*.sh"
+zip -r ../../archives/sportspress-pro.$version.zip ../sportspress-pro -x "*.DS_Store" ".tx/*" "*.sh"
