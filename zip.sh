d87751907dba9bae525e1e7e9172ebb53737e002
Brian Miyaji <brian@themeboy.com>
2016-01-20 13:55:23 +1100

    Remove agency class

--- zip.sh
+++ zip.sh
@@ -3,7 +3,4 @@ read version
 
 mkdir ../../archives/sportspress-pro/$version
 
-zip -r ../../archives/sportspress-pro/$version/sportspress-pro.agency.$version.zip . -x "*.DS_Store" ".tx/*" "*.sh"
-zip -r ../../archives/sportspress-pro/$version/sportspress-pro.league.$version.zip . -x "*.DS_Store" ".tx/*" "*.sh" "includes/sportspress-agency/*"
-zip -r ../../archives/sportspress-pro/$version/sportspress-pro.club.$version.zip . -x "*.DS_Store" ".tx/*" "*.sh" "includes/sportspress-agency/*" "includes/sportspress-multisite/*"
-zip -r ../../archives/sportspress-pro/$version/sportspress-pro.social.$version.zip . -x "*.DS_Store" ".tx/*" "*.sh" "includes/sportspress-agency/*" "includes/sportspress-multisite/*" "includes/sportspress-lazy-loading/*" "includes/sportspress-sponsors/*" "includes/sportspress-staff-directories/*" "includes/sportspress-team-access/*" "includes/sportspress-tournaments/*"
+zip -r ../../archives/sportspress-pro/$version/sportspress-pro.zip . -x "*.DS_Store" ".tx/*" "*.sh"
