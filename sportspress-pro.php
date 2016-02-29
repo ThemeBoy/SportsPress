433b8ff43ca2bdd48b25ec91ce6980e97c0d95d3
Brian Miyaji <brian@themeboy.com>
2016-02-29 16:33:07 +1100

    Update pro version too

--- sportspress-pro.php
+++ sportspress-pro.php
@@ -3,7 +3,7 @@
  * Plugin Name: SportsPress Pro
  * Plugin URI: http://tboy.co/pro
  * Description: Advanced club & league management.
- * Version: 1.9.17
+ * Version: 1.9.19
  * Author: ThemeBoy
  * Author URI: http://themeboy.com
  * Requires at least: 3.8
@@ -26,14 +26,14 @@ if ( ! class_exists( 'SportsPress_Pro' ) ) :
  * Main SportsPress Pro Class
  *
  * @class SportsPress_Pro
- * @version	1.9.17
+ * @version	1.9.19
  */
 final class SportsPress_Pro {
 
 	/**
 	 * @var string
 	 */
-	public $version = '1.9.17';
+	public $version = '1.9.19';
 
 	/**
 	 * SportsPress Pro Constructor.
