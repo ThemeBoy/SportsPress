ff666d9a617457193393472d6d6cb387ed893eb7
Brian Miyaji <brian@themeboy.com>
2016-04-04 14:54:06 +1000

    Tweak scoreboard js

--- includes/sportspress-scoreboards/js/sportspress-scoreboards.js
+++ includes/sportspress-scoreboards/js/sportspress-scoreboards.js
@@ -1,46 +1,46 @@
 jQuery(document).ready(function($){
-	$(".sp-scoreboard-nav").each(function() {
-		$parent = $(this).siblings(".sp-scoreboard-content");
+	$(".sp-scoreboard-content").each(function() {
+		$parent = $(this);
 		$el = $parent.find(".sp-scoreboard");
 		if ( $el.outerWidth() <= $parent.innerWidth() ) {
 			$(this).closest(".sp-scoreboard-wrapper").find(".sp-scoreboard-next").addClass("sp-scoreboard-nav-disabled");
 		}
-		
-		$(this).click(function() {
-			step = $(this).data("sp-step");
-			if ( ! step ) step = 100;
-			$wrapper = $(this).closest(".sp-scoreboard-wrapper");
-			$parent = $(this).siblings(".sp-scoreboard-content");
-			$el = $parent.find(".sp-scoreboard");
-			if ( $el.is(":animated") ) return false;
-			left = $el.position().left;
-			if ( $(this).hasClass("sp-scoreboard-prev") ) {
-				$(this).closest(".sp-scoreboard-wrapper").find(".sp-scoreboard-next").removeClass("sp-scoreboard-nav-disabled");
-				if ( left >= $wrapper.innerWidth() - $parent.outerWidth() - step ) {
-					$(this).closest(".sp-scoreboard-wrapper").find(".sp-scoreboard-prev").addClass("sp-scoreboard-nav-disabled");
-					$el.animate({
-						left : 0
-					});
-				} else {
-					$el.animate({
-						left : "+=" + step
-					});
-				}
+	});
+	
+	$(".sp-scoreboard-nav").click(function() {
+		step = $(this).data("sp-step");
+		if ( ! step ) step = 100;
+		$wrapper = $(this).closest(".sp-scoreboard-wrapper");
+		$parent = $(this).siblings(".sp-scoreboard-content");
+		$el = $parent.find(".sp-scoreboard");
+		if ( $el.is(":animated") ) return false;
+		left = $el.position().left;
+		if ( $(this).hasClass("sp-scoreboard-prev") ) {
+			$(this).closest(".sp-scoreboard-wrapper").find(".sp-scoreboard-next").removeClass("sp-scoreboard-nav-disabled");
+			if ( left >= $wrapper.innerWidth() - $parent.outerWidth() - step * 2 ) {
+				$(this).closest(".sp-scoreboard-wrapper").find(".sp-scoreboard-prev").addClass("sp-scoreboard-nav-disabled");
+				$el.animate({
+					left : 0
+				});
 			} else {
-				$(this).closest(".sp-scoreboard-wrapper").find(".sp-scoreboard-prev").removeClass("sp-scoreboard-nav-disabled");
-				overflow = $el.outerWidth() - $parent.innerWidth() + left;
-				if ( overflow <= step ) {
-					$(this).closest(".sp-scoreboard-wrapper").find(".sp-scoreboard-next").addClass("sp-scoreboard-nav-disabled");
-					$el.animate({
-						left : $parent.innerWidth() - $el.outerWidth()
-					});
-				} else {
-					$el.animate({
-						left : "-=" + step
-					});
-				}
+				$el.animate({
+					left : "+=" + step
+				});
 			}
-			return false;
-		});
+		} else {
+			$(this).closest(".sp-scoreboard-wrapper").find(".sp-scoreboard-prev").removeClass("sp-scoreboard-nav-disabled");
+			overflow = $el.outerWidth() - $parent.innerWidth() + left;
+			if ( overflow <= step * 2 ) {
+				$(this).closest(".sp-scoreboard-wrapper").find(".sp-scoreboard-next").addClass("sp-scoreboard-nav-disabled");
+				$el.animate({
+					left : $parent.innerWidth() - $el.outerWidth()
+				});
+			} else {
+				$el.animate({
+					left : "-=" + step
+				});
+			}
+		}
+		return false;
 	});
 });
