ae6d6121aabb4669d67285a7be08835d5706ea34
Brian Miyaji <brian@themeboy.com>
2016-04-08 15:27:52 +1000

    Limit venue archives to events

--- includes/sp-template-hooks.php
+++ includes/sp-template-hooks.php
@@ -75,12 +75,14 @@ add_action( 'sportspress_before_single_team', 'sportspress_output_post_excerpt',
  * @see sportspress_output_team_staff()
  * @see sportspress_output_team_lists()
  * @see sportspress_output_team_tables()
+ * @see sportspress_output_team_events()
  */
 add_action( 'sportspress_single_team_content', 'sportspress_output_team_link', 0 );
 add_action( 'sportspress_single_team_content', 'sportspress_output_team_details', 10 );
 add_action( 'sportspress_single_team_content', 'sportspress_output_team_staff', 15 );
 add_action( 'sportspress_single_team_content', 'sportspress_output_team_lists', 20 );
 add_action( 'sportspress_single_team_content', 'sportspress_output_team_tables', 30 );
+add_action( 'sportspress_single_team_content', 'sportspress_output_team_events', 40 );
 
 /**
  * Single Table Content
@@ -305,7 +307,8 @@ function sportspress_pre_get_posts( $query ) {
 		endif;
 	else:
 		if ( isset( $query->query[ 'sp_venue' ] ) ):
-        	$GLOBALS[ 'wp_post_statuses' ][ 'future' ]->public = true;
+			$query->set( 'post_type', 'sp_event' );
+			$GLOBALS[ 'wp_post_statuses' ][ 'future' ]->public = true;
 		endif;
 	endif;
 
