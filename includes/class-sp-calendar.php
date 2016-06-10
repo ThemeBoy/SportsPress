803716d68e9cec685825fad11327d64376e8d94a
Brian Miyaji <brian@themeboy.com>
2016-06-10 11:45:49 +1000

    Tag version 2.0.8

--- includes/class-sp-calendar.php
+++ includes/class-sp-calendar.php
@@ -5,7 +5,7 @@
  * The SportsPress calendar class handles individual calendar data.
  *
  * @class 		SP_Calendar
- * @version     2.0
+ * @version     2.0.8
  * @package		SportsPress/Classes
  * @category	Class
  * @author 		ThemeBoy
