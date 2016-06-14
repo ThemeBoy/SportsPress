faf761a38dec00d2cc5b41ee9ee1a899538a44b9
Brian Miyaji <brian@themeboy.com>
2016-06-14 16:24:28 +1000

    Update core

---
+++ includes/sportspress/includes/admin/views/html-notice-no-access.php
@@ -0,0 +1,6 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+?>
+<div id="message" class="error">
+	<p><?php _e( 'You are not allowed to edit this item.', 'sportspress' ); ?></p>
+</div>
