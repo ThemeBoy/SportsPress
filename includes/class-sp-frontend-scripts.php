3d91a230c20d5bf889b70717632beac66db8bd6b
Brian Miyaji <brian@themeboy.com>
2016-07-08 02:34:38 +1000

    Update maps

--- includes/class-sp-frontend-scripts.php
+++ includes/class-sp-frontend-scripts.php
@@ -75,12 +75,6 @@ class SP_Frontend_Scripts {
 		wp_enqueue_script( 'jquery-countdown', plugin_dir_url( SP_PLUGIN_FILE ) .'assets/js/jquery.countdown.min.js', array( 'jquery' ), '2.0.2', true );
 		wp_enqueue_script( 'sportspress', plugin_dir_url( SP_PLUGIN_FILE ) .'assets/js/sportspress.js', array( 'jquery' ), SP()->version, true );
 
-		if ( is_singular( 'sp_event' ) || is_tax( 'sp_venue' ) ):
-			wp_enqueue_script( 'google-maps', '//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false', array(), '3.exp', true );
-			wp_enqueue_script( 'sp-maps', plugin_dir_url( SP_PLUGIN_FILE ) .'assets/js/sp-maps.js', array( 'jquery', 'google-maps' ), time(), true );
-			wp_localize_script( 'sp-maps', 'vars', array( 'map_type' => strtoupper( get_option( 'sportspress_map_type', 'ROADMAP' ) ), 'zoom' => get_option( 'sportspress_map_zoom', 15 ) ) );
-		endif;
-
 		// Localize scripts
 		wp_localize_script( 'sportspress', 'localized_strings', array( 'days' => __( 'days', 'sportspress' ), 'hrs' => __( 'hrs', 'sportspress' ), 'mins' => __( 'mins', 'sportspress' ), 'secs' => __( 'secs', 'sportspress' ), 'previous' => __( 'Previous', 'sportspress' ), 'next' => __( 'Next', 'sportspress' ) ) );
 
--- templates/venue-map.php
+++ templates/venue-map.php
@@ -2,22 +2,33 @@
 /**
  * Venue Map
  *
- * @author 		ThemeBoy
- * @package 	SportsPress/Templates
+ * @author      ThemeBoy
+ * @package     SportsPress/Templates
  * @version     0.8
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 if ( ! isset( $meta ) )
-	return;
+    return;
 
 $address = sp_array_value( $meta, 'sp_address', null );
+$address = urlencode( $address );
 $latitude = sp_array_value( $meta, 'sp_latitude', null );
 $longitude = sp_array_value( $meta, 'sp_longitude', null );
+$zoom = get_option( 'sportspress_map_zoom', 15 );
+$maptype = get_option( 'sportspress_map_type', 'roadmap' );
+$maptype = strtolower( $maptype );
+if ( 'satellite' !== $maptype ) $maptype = 'roadmap';
 
 if ( $latitude != null && $longitude != null ):
-	?>
-	<div class="sp-google-map<?php if ( is_tax( 'sp_venue' ) ): ?> sp-venue-map<?php endif; ?>" data-address="<?php echo $address; ?>" data-latitude="<?php echo $latitude; ?>" data-longitude="<?php echo $longitude; ?>"></div>
-	<?php
+    ?>
+    <iframe
+      class="sp-google-map<?php if ( is_tax( 'sp_venue' ) ): ?> sp-venue-map<?php endif; ?>"
+      width="600"
+      height="320"
+      frameborder="0" style="border:0"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAWyt_AG0k_Pgz4LuegtHwesA_OMRnSSAE&amp;q=<?php echo $address; ?>&amp;center=<?php echo $latitude; ?>,<?php echo $longitude; ?>&amp;zoom=<?php echo $zoom; ?>" allowfullscreen>
+    </iframe>
+    <?php
 endif;
