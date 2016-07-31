5129b95eee33105f6db2f3e4534c641de98c1eee
Brian Miyaji <brian@themeboy.com>
2016-07-31 16:17:36 +1000

    Tag version 2.0.14

--- templates/venue-map.php
+++ templates/venue-map.php
@@ -4,7 +4,7 @@
  *
  * @author      ThemeBoy
  * @package     SportsPress/Templates
- * @version     2.0.13
+ * @version     2.0.14
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
