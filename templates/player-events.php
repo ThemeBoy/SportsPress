f4f1ce79a40119c788d22b13768fad57b87be1f7
Brian Miyaji <brianmiyaji@192-168-1-3.tpgi.com.au>
2016-12-19 21:24:53 +1100

    Show recent events first in player and team profiles

--- templates/player-events.php
+++ templates/player-events.php
@@ -16,6 +16,6 @@ $format = get_option( 'sportspress_player_events_format', 'blocks' );
 if ( 'calendar' === $format )
 	sp_get_template( 'event-calendar.php', array( 'player' => $id ) );
 elseif ( 'list' === $format )
-	sp_get_template( 'event-list.php', array( 'player' => $id, 'title_format' => 'homeaway', 'time_format' => 'separate', 'columns' => array( 'event', 'time', 'results' ) ) );
+	sp_get_template( 'event-list.php', array( 'player' => $id, 'order' => 'DESC', 'title_format' => 'homeaway', 'time_format' => 'separate', 'columns' => array( 'event', 'time', 'results' ) ) );
 else
 	sp_get_template( 'event-fixtures-results.php', array( 'player' => $id ) );
--- templates/team-events.php
+++ templates/team-events.php
@@ -16,6 +16,6 @@ $format = get_option( 'sportspress_team_events_format', 'blocks' );
 if ( 'calendar' === $format )
 	sp_get_template( 'event-calendar.php', array( 'team' => $id ) );
 elseif ( 'list' === $format )
-	sp_get_template( 'event-list.php', array( 'team' => $id, 'title_format' => 'homeaway', 'time_format' => 'separate', 'columns' => array( 'event', 'time', 'results' ) ) );
+	sp_get_template( 'event-list.php', array( 'team' => $id, 'order' => 'DESC', 'title_format' => 'homeaway', 'time_format' => 'separate', 'columns' => array( 'event', 'time', 'results' ) ) );
 else
 	sp_get_template( 'event-fixtures-results.php', array( 'team' => $id ) );
