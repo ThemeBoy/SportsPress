3486e5a53f9067ba00a7324e82c61d900dbaa1d7
Brian Miyaji <brian@themeboy.com>
2017-01-06 22:29:49 +1100

    Add timelines module

---
+++ includes/sportspress-timelines/sportspress-timelines.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Plugin Name: SportsPress Timelines
+Plugin URI: http://tboy.co/pro
+Description: Add timelines to SportsPress events.
+Author: ThemeBoy
+Author URI: http://themeboy.com
+Version: 2.1.7
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists( 'SportsPress_Timelines' ) ) :
+
+/**
+ * Main SportsPress Timelines Class
+ *
+ * @class SportsPress_Timelines
+ * @version	2.1.7
+ *
+ */
+class SportsPress_Timelines {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		// Define constants
+		$this->define_constants();
+
+		// Hooks
+		add_filter( 'sportspress_event_templates', array( $this, 'templates' ) );
+	    add_filter( 'sportspress_enqueue_styles', array( $this, 'add_styles' ) );
+	}
+
+	/**
+	 * Define constants
+	 */
+	private function define_constants() {
+		if ( !defined( 'SP_TIMELINES_VERSION' ) )
+			define( 'SP_TIMELINES_VERSION', '2.1.7' );
+
+		if ( !defined( 'SP_TIMELINES_URL' ) )
+			define( 'SP_TIMELINES_URL', plugin_dir_url( __FILE__ ) );
+
+		if ( !defined( 'SP_TIMELINES_DIR' ) )
+			define( 'SP_TIMELINES_DIR', plugin_dir_path( __FILE__ ) );
+	}
+
+	/**
+	 * Add templates to event layout.
+	 *
+	 * @return array
+	 */
+	public function templates( $templates = array() ) {
+		$templates['timeline'] = array(
+			'title' => __( 'Timeline', 'sportspress' ),
+			'option' => 'sportspress_event_show_timeline',
+			'action' => array( $this, 'output' ),
+			'default' => 'yes',
+		);
+		
+		return $templates;
+	}
+
+	/**
+	 * Output timeline.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function output() {
+		sp_get_template( 'event-timeline.php', array(), '', SP_TIMELINES_DIR . 'templates/' );
+	}
+
+	/**
+	 * Add styles to SP frontend
+	 */
+	public function add_styles( $styles = array() ) {
+		$styles['sportspress-timelines'] = array(
+			'src'     => str_replace( array( 'http:', 'https:' ), '', SP_TIMELINES_URL ) . 'css/sportspress-timelines.css',
+			'deps'    => 'sportspress-general',
+			'version' => SP_TIMELINES_VERSION,
+			'media'   => 'all'
+		);
+		return $styles;
+	}
+}
+
+endif;
+
+if ( get_option( 'sportspress_load_timelines_module', 'yes' ) == 'yes' ) {
+	new SportsPress_Timelines();
+}
---
+++ includes/sportspress-timelines/templates/event-timeline.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Timeline
+ *
+ * @author 		ThemeBoy
+ * @package 	SportsPress_Timelines
+ * @version     2.1.7
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if ( ! isset( $id ) )
+	$id = get_the_ID();
+
+$event = new SP_Event( $id );
+$timeline = $event->timeline( false, true );
+if ( empty( $timeline ) ) return;
+
+$link_posts = get_option( 'sportspress_link_players', 'yes' ) == 'yes' ? true : false;
+
+$minutes = $event->minutes();
+
+$previous = 0;
+
+?>
+<div class="sp-template sp-template-timeline sp-template-event-timeline">
+	<div class="sp-timeline">
+		<hr>
+		<?php foreach ( $timeline as $minute => $details ) { ?>
+			<?php
+			$time = sp_array_value( $details, 'time', false );
+			if ( false === $time ) continue;
+
+			$name = sp_array_value( $details, 'name', __( 'Player', 'sportspress' ) );
+			$number = sp_array_value( $details, 'number', '' );
+			$icon = sp_array_value( $details, 'icon', '' );
+			$side = sp_array_value( $details, 'side', 'home' );
+
+			if ( '' !== $number ) $name = $number . '. ' . $name;
+
+			$permalink = get_post_permalink( $details['id'] );
+
+			$offset = floor( $time / $minutes * 100 );
+			if ( $offset - $previous <= 2 ) $offset = $previous + 2;
+			$previous = $offset;
+			?>
+			<span class="sp-timeline-minute sp-timeline-minute-<?php echo $side; ?>" title="<?php echo $name; ?>" style="left: <?php echo $offset; ?>%;">
+				<span class="sp-timeline-icon sp-timeline-icon-<?php echo $side; ?>"><?php echo $icon; ?></span>
+				<?php echo $time; ?>
+			</span>
+		<?php } ?>
+	</div>
+</div>
