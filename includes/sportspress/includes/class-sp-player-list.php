b359121d8cc0eabceb546f9abad6a30a70952580
Brian Miyaji <brian@themeboy.com>
2017-01-13 10:21:46 +1100

    Update core

--- includes/sportspress/includes/class-sp-player-list.php
+++ includes/sportspress/includes/class-sp-player-list.php
@@ -5,7 +5,7 @@
  * The SportsPress player list class handles individual player list data.
  *
  * @class 		SP_Player_List
- * @version     2.2.2
+ * @version     2.2.3
  * @package		SportsPress/Classes
  * @category	Class
  * @author 		ThemeBoy
@@ -526,8 +526,10 @@ class SP_Player_List extends SP_Custom_Post {
 					endif;
 				endif;
 
-				if ( $placeholder !== '' ):
+				if ( $placeholder !== '' && is_numeric( $placeholder ) ):
 					$placeholders[ $player_id ][ $stat->post_name ] = sp_array_value( $placeholders[ $player_id ], $stat->post_name, 0 ) + $placeholder;
+				else:
+					$placeholders[ $player_id ][ $stat->post_name ] = sp_array_value( $placeholders[ $player_id ], $stat->post_name, '-' );
 				endif;
 			endforeach;
 
