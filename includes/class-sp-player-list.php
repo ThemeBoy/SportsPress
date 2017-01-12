12b22268179cd7cae037a67a61ba8277ac69438d
Brian Miyaji <brian@themeboy.com>
2017-01-13 07:39:15 +1100

    Fix foreach error in player lists

--- includes/class-sp-player-list.php
+++ includes/class-sp-player-list.php
@@ -326,8 +326,8 @@ class SP_Player_List extends SP_Custom_Post {
 										$totals[ $player_id ]['eventminutes'] -= sp_array_value( sp_array_value( sp_array_value( sp_array_value( $timeline, $team_id ), $player_id ), 'sub' ), 0, 0 );
 									else:
 										foreach ( $timeline as $timeline_team => $timeline_players ):
+											if ( ! is_array( $timeline_players ) ) continue;
 											foreach ( $timeline_players as $timeline_player => $timeline_performance ):
-												if ( ! is_array( $timeline_performance ) ) continue;
 												if ( 'sub' === sp_array_value( sp_array_value( $players, $timeline_player, array() ), 'status' ) && $player_id === (int) sp_array_value( sp_array_value( $players, $timeline_player, array() ), 'sub', 0 ) ):
 													$substitution_time = sp_array_value( sp_array_value( sp_array_value( sp_array_value( $timeline, $team_id ), $timeline_player ), 'sub' ), 0, 0 );
 													if ( $substitution_time ):
