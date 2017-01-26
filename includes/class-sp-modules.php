dd503a7873aba0898a51f5173f01e48a27f7d44d
Brian Miyaji <brian@themeboy.com>
2017-01-26 14:25:40 +1100

    Tag version 2.2.7

--- includes/class-sp-modules.php
+++ includes/class-sp-modules.php
@@ -5,7 +5,7 @@
  * The SportsPress modules class stores available modules.
  *
  * @class 		SP_Modules
- * @version     2.2
+ * @version   2.2.7
  * @package		SportsPress/Classes
  * @category	Class
  * @author 		ThemeBoy
