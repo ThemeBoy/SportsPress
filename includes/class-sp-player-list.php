fd75c66fb180708bce130696a4ba23285c5fd381
Brian Miyaji <brian@themeboy.com>
2017-01-31 23:30:34 +1100

    Move placeholders array assignment outside of conditional

--- includes/class-sp-player-list.php
+++ includes/class-sp-player-list.php
@@ -560,9 +560,11 @@ class SP_Player_List extends SP_Custom_Post {
 					$placeholder = sp_array_value( $placeholders[ $player_id ], $stat->post_name, '-' );
 				endif;
 
-				if ( is_numeric( $placeholder ) ):
-					$placeholders[ $player_id ][ $stat->post_name ] = number_format( $placeholder, $stat->precision, '.', '' );
+				if ( is_numeric( $placeholder ) && $stat->precision ):
+					$placeholder = number_format( $placeholder, $stat->precision, '.', '' );
 				endif;
+
+				$placeholders[ $player_id ][ $stat->post_name ] = $placeholder;
 			endforeach;
 
 		endforeach;
