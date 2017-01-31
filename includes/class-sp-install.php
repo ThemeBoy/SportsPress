99b5641d9cc7366fb0f9fa3197d0a9d0dfaa807e
Brian Miyaji <brian@themeboy.com>
2017-01-31 19:30:52 +1100

    Tag version 2.2.8

--- includes/class-sp-install.php
+++ includes/class-sp-install.php
@@ -5,7 +5,7 @@
  * @author 		ThemeBoy
  * @category 	Admin
  * @package 	SportsPress/Classes
- * @version     2.2.4
+ * @version     2.2.8
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
