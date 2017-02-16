4b42e124dedbc2c4aefc0c4d892d1fdb13dfe10c
Jeremy Herve <jeremy@tagada.hu>
2017-02-16 19:19:18 +0100

    Avoid Fatal Errors when Core REST API classes don't exist.

--- includes/api/class-sp-rest-posts-controller.php
+++ includes/api/class-sp-rest-posts-controller.php
@@ -1,8 +1,10 @@
 <?php
 
-class SP_REST_Posts_Controller extends WP_REST_Posts_Controller {
-	public function __construct( $post_type ) {
-		parent::__construct( $post_type );
-		$this->namespace = 'sportspress/v2';
+if ( class_exists( 'WP_REST_Posts_Controller' ) ) {
+	class SP_REST_Posts_Controller extends WP_REST_Posts_Controller {
+		public function __construct( $post_type ) {
+			parent::__construct( $post_type );
+			$this->namespace = 'sportspress/v2';
+		}
 	}
 }
