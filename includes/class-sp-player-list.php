99785a7a82d8bc3b5ad2385091faba4df48fcffd
Brian Miyaji <brian@themeboy.com>
2018-05-03 16:18:01 +1000

    Revert "Always load player assignments and rename post meta"
    This reverts commit 427b18052d4eb8ae9bc436577f8c9ed79423f416.

--- includes/class-sp-player-list.php
+++ includes/class-sp-player-list.php
@@ -115,7 +115,7 @@ class SP_Player_List extends SP_Secondary_Post {
 			if ( !empty( $assignments ) ) {
 				$args['meta_query'] = array(
 					array(
-						'key' => 'sp_assignments',
+						'key' => 'sp_player_assignments',
 						'value' => $assignments,
 						'compare' => 'IN'
 					),
