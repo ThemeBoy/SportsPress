d83abf4b120b8380c5e5bb03da65858ed860f487
savvasha <savvasha@hotmail.com>
2018-09-15 17:02:54 +0300

    Avoid PHP warnings with PHP >= 7.2.x
    Fix PHP Warning:  sizeof(): Parameter must be an array or an object that implements Countable on line 24

--- includes/admin/post-types/meta-boxes/class-sp-meta-box-player-statistics.php
+++ includes/admin/post-types/meta-boxes/class-sp-meta-box-player-statistics.php
@@ -21,7 +21,11 @@ class SP_Meta_Box_Player_Statistics {
 	public static function output( $post ) {
 		$player = new SP_Player( $post );
 		$leagues = $player->get_terms_sorted_by_sp_order( 'sp_league' );
-		$league_num = sizeof( $leagues );
+		if ( is_array ( $leagues ) ) {
+			$league_num = sizeof( $leagues );
+		}else{
+			$league_num =0;
+		}
 		$sections = get_option( 'sportspress_player_performance_sections', -1 );
 		$show_career_totals = 'yes' === get_option( 'sportspress_player_show_career_total', 'no' ) ? true : false;
 
