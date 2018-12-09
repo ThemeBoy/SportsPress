ea19cc4ec9b0ef9bcc04bd2b2f8400da85d15f8f
Brian Miyaji <brian@themeboy.com>
2018-12-09 11:50:07 +1100

    Fix calculated performance totals missing from array for custom templates

--- includes/class-sp-event.php
+++ includes/class-sp-event.php
@@ -184,7 +184,7 @@ class SP_Event extends SP_Custom_Post{
 					if ( ! is_array( $players ) ) continue;
 
 					foreach ( $players as $player => $player_performance ):
-						if ( ! is_array( $player_performance ) || ! $player ) continue;
+						if ( ! is_array( $player_performance ) ) continue;
 
 						// Prepare existing values for equation calculation
 						$vars = $player_performance;
