f35479109495497b8691ebbf20a08e6f1e0c469d
Nabil Kadimi <nabil@kadimi.com>
2019-03-30 14:32:30 +0100

    Use the right domain in load_plugin_textdomain and mo files

--- sportspress-pro.php
+++ sportspress-pro.php
@@ -144,7 +144,7 @@ final class SportsPress_Pro {
 		$locale = apply_filters( 'plugin_locale', get_locale(), 'sportspress' );
 
 		// Global + Frontend Locale
-		load_plugin_textdomain( 'sportspress-pro', false, plugin_basename( dirname( __FILE__ ) . "/languages" ) );
+		load_plugin_textdomain( 'sportspress', false, plugin_basename( dirname( __FILE__ ) . "/languages" ) );
 	}
 
 	/**
