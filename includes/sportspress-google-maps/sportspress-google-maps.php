721212d8c36f1b8c4499621d21e373684dd1c88c
Brian Miyaji <brian@themeboy.com>
2019-05-28 16:46:06 +1000

    Use hooks to load Google Maps scripts

--- includes/sportspress-google-maps/sportspress-google-maps.php
+++ includes/sportspress-google-maps/sportspress-google-maps.php
@@ -29,8 +29,15 @@ class SportsPress_Google_Maps {
 		$this->define_constants();
 
 		// Hooks
-		add_action( 'sp_venue_show_google_maps', array( $this, 'show_venue_google_maps' ), 10, 5 );
+		remove_all_actions( 'sp_venue_show_map', 10 );
+		remove_all_actions( 'sp_admin_geocoder_scripts', 10 );
+		remove_all_actions( 'sp_setup_geocoder_scripts', 10 );
+		remove_all_actions( 'sp_setup_venue_geocoder_scripts', 10 );
+		add_action( 'sp_venue_show_map', array( $this, 'show_venue_map' ), 10, 5 );
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+		add_action( 'sp_admin_geocoder_scripts', array( $this, 'admin_geocoder_scripts' ), 10 );
+		add_action( 'sp_setup_geocoder_scripts', array( $this, 'setup_geocoder_scripts' ), 10 );
+		add_action( 'sp_setup_venue_geocoder_scripts', array( $this, 'setup_venue_geocoder_scripts' ), 10 );
 	}
 
 	/**
@@ -53,7 +60,7 @@ class SportsPress_Google_Maps {
 	 *
 	 * @return mix
 	 */
-	public function show_venue_google_maps( $latitude, $longitude, $address, $zoom, $maptype ) { ?>
+	public function show_venue_map( $latitude, $longitude, $address, $zoom, $maptype ) { ?>
 		<div class="sp-google-map-container">
 		  <iframe
 			class="sp-google-map<?php if ( is_tax( 'sp_venue' ) ): ?> sp-venue-map<?php endif; ?>"
@@ -83,11 +90,33 @@ class SportsPress_Google_Maps {
 			wp_enqueue_script( 'sportspress-admin-locationpicker' );
 		}
 	}
-
+	
+	/**
+	 * Print geocoder script in admin
+	 */
+	public function admin_geocoder_scripts() {
+		wp_print_scripts( 'sportspress-admin-locationpicker' ); 
+	}
+	
+	/**
+	 * Print geocoder script in setup
+	 */
+	public function setup_geocoder_scripts() {
+    wp_register_script( 'google-maps', '//tboy.co/maps_js' );
+    wp_register_script( 'jquery-locationpicker', SP_GOOGLE_MAPS_URL . 'js/locationpicker.jquery.js', array( 'jquery', 'google-maps' ), '0.1.6', true );
+    wp_register_script( 'sportspress-admin-locationpicker', SP_GOOGLE_MAPS_URL . 'js/admin/locationpicker.js', array( 'jquery', 'google-maps', 'jquery-locationpicker' ), SP_GOOGLE_MAPS_VERSION, true );
+	}
+	
+	/**
+	 * Print geocoder script in setup venue step
+	 */
+	public function setup_venue_geocoder_scripts() {
+		wp_print_scripts( 'google-maps' );
+	}
 }
 
 endif;
 
-if ( get_option( 'sportspress_load_google_maps_module', 'no' ) == 'yes' ) {
+if ( get_option( 'sportspress_load_google_maps_module', 'yes' ) == 'yes' ) {
 	new SportsPress_Google_Maps();
 }
