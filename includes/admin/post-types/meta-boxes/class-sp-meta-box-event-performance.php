bc1f5697f7d2774dea518ac1407521baed401b48
savvasha <savvasha@hotmail.com>
2020-06-30 09:34:47 +0300

    Avoid showing minutes box for subs when not needed

--- includes/admin/post-types/meta-boxes/class-sp-meta-box-event-performance.php
+++ includes/admin/post-types/meta-boxes/class-sp-meta-box-event-performance.php
@@ -503,8 +503,10 @@ class SP_Meta_Box_Event_Performance {
 					?>
 					<?php echo self::status_select( $team_id, $player_id, sp_array_value( $player_performance, 'status', null ) ); ?><br>
 					<?php echo self::sub_select( $team_id, $player_id, sp_array_value( $player_performance, 'sub', null ), $data ); ?><br>
-					<input class="sp-sync-input small-text" type="text" name="sp_timeline[<?php echo $team_id; ?>][<?php echo $player_id; ?>][sub][]" value="<?php echo esc_attr( sp_array_value( $times, 0, '' ) ); ?>" placeholder="-" />
-					<span class="description"><?php _e( 'mins', 'sportspress' ); ?></span>
+					<?php if ( is_array( $times ) ) { ?>
+						<input class="sp-sync-input small-text" type="text" name="sp_timeline[<?php echo $team_id; ?>][<?php echo $player_id; ?>][sub][]" value="<?php echo esc_attr( sp_array_value( $times, 0, '' ) ); ?>" placeholder="-" />
+						<span class="description"><?php _e( 'mins', 'sportspress' ); ?></span>
+					<?php } ?>
 				</td>
 			<?php } ?>
 			<?php if ( $stars_type ) { ?>
