d72285be3d8bc817506631518e1b4899a39968d1
savvasha <savvasha@hotmail.com>
2020-09-06 18:31:46 +0300

    Make the fixtures export to csv actually working

--- includes/admin/class-sp-admin-exporters.php
+++ includes/admin/class-sp-admin-exporters.php
@@ -16,13 +16,16 @@ if ( ! class_exists( 'SP_Admin_Exporters' ) ) :
  * SP_Admin_Exporters Class
  */
 class SP_Admin_Exporters {
+	
+	/** @var array The export pages. */
+	public $export_pages;
 
-	/**
-	 * Hook in tabs.
-	 */
 	public function __construct() {
 		add_action( 'admin_print_footer_scripts', array( $this, 'action_links' ) );
 		add_action( 'admin_menu', array( $this, 'register_sub_menu' ) );
+		add_action( 'admin_init', array( $this, 'download_exported_file' ) );
+		
+		$this->export_pages = array ( 'sp_event_exporter', 'sp_fixture_exporter', 'sp_team_exporter', 'sp_player_exporter', 'sp_staff_exporter' );
 	}
 
 	/**
@@ -143,6 +146,111 @@ class SP_Admin_Exporters {
 		require 'exporters/class-sp-staff-exporter.php';
 	}
 	
+	/**
+	 * Download Exported file 
+	 */
+	public function download_exported_file() {
+		global $plugin_page;
+		if ( in_array( $plugin_page, $this->export_pages ) && isset( $_POST['submit'] ) ) {
+			
+			function outputCsv( $fileName, $assocDataArray ) {
+				ob_clean();
+				header( 'Pragma: public' );
+				header( 'Expires: 0' );
+				header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
+				header( 'Cache-Control: private', false );
+				header( 'Content-Type: text/csv' );
+				header( 'Content-Disposition: attachment;filename=' . $fileName );
+				if ( isset( $assocDataArray[0] ) ) {
+					$fp = fopen( 'php://output', 'w' );
+					fputcsv( $fp, array_keys( $assocDataArray[0] ) );
+					foreach ( $assocDataArray AS $values ) {
+						fputcsv( $fp, $values );
+					}
+					fclose( $fp );
+				}
+				ob_flush();
+			}
+			
+			switch ( $plugin_page ) {
+			  case 'sp_event_exporter':
+				echo 'sp_event_exporter';
+				break;
+			  case 'sp_fixture_exporter':
+				$fixtures = $this->sp_fixtures_data();
+				outputCsv( 'sp_fixtures_' . time() . '.csv', $fixtures );
+				break;
+			  case 'sp_team_exporter':
+				echo 'sp_team_exporter';
+				break;
+			  case 'sp_player_exporter':
+				echo 'sp_player_exporter';
+				break;
+			  case 'sp_staff_exporter':
+				echo 'sp_staff_exporter';
+				break;
+			  default:
+				echo 'error';
+			}
+			
+			exit;
+		}
+	}
+	
+	
+	/**
+	* Generate fixtures data
+	*/
+	public function sp_fixtures_data() {
+		$args = array(
+			'post_type' => 'sp_event',
+			'posts_per_page' => -1,
+			'meta_query' => array(
+				'relation' => 'AND'
+			),
+			'tax_query' => array(
+				'relation' => 'AND'
+			),
+		);
+		
+		if ( $_POST['sp_league'] != "-1" ) {
+			$args['tax_query'][] = array(
+						'taxonomy' => 'sp_league',
+						'field' => 'slug',
+						'terms' => $_POST['sp_league']
+					);
+		}
+		if ( $_POST['sp_season'] != "-1" ) {
+			$args['tax_query'][] = array(
+						'taxonomy' => 'sp_season',
+						'field' => 'slug',
+						'terms' => $_POST['sp_season']
+					);
+		}
+		$events = get_posts( $args );
+		$events_array = array();
+		$i = 0;
+		if ( $events ) {
+			foreach ( $events as $event ) {
+				$events_array[$i]['event_id'] = $event->ID; //team_id
+				$events_array[$i]['date'] = get_the_date( 'Y/m/d', $event ); //date
+				$events_array[$i]['time'] = get_the_date( 'H:i:s', $event ); //time
+				$teams = get_post_meta ( $event->ID, 'sp_team' );
+				$events_array[$i]['home'] = get_the_title( $teams[0] ); //home
+				$events_array[$i]['away'] = get_the_title( $teams[1] ); //away
+				$venues = get_the_terms( $event->ID, 'sp_venue' );
+				if ( $venues ) {
+					$venue = $venues[0]->name;
+				}else{
+					$venue = '';
+				}
+				$events_array[$i]['venue'] = $venue; //venue
+				$events_array[$i]['day'] = get_post_meta ( $event->ID, 'sp_day', true ); //day
+				$i++;
+			}
+		}
+		return $events_array;
+	}
 }
 
 endif;
--- includes/admin/exporters/class-sp-fixture-exporter.php
+++ includes/admin/exporters/class-sp-fixture-exporter.php
@@ -9,75 +9,7 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-if ( $_POST && isset( $_POST['submit'] ) ) {
-	$args = array(
-			'post_type' => 'sp_event',
-			'posts_per_page' => -1,
-			'meta_query' => array(
-				'relation' => 'AND'
-			),
-			'tax_query' => array(
-				'relation' => 'AND'
-			),
-		);
-	if ( $_POST['sp_league'] != "-1" ) {
-		$args['tax_query'][] = array(
-					'taxonomy' => 'sp_league',
-					'field' => 'slug',
-					'terms' => $_POST['sp_league']
-				);
-	}
-	if ( $_POST['sp_season'] != "-1" ) {
-		$args['tax_query'][] = array(
-					'taxonomy' => 'sp_season',
-					'field' => 'slug',
-					'terms' => $_POST['sp_season']
-				);
-	}
-	$events = get_posts( $args );
-	if ( $events ) {
-		$number_of_events = count ( $events );
-		ob_start();
-		$df = fopen("php://output", 'w');
-		$events_array[] = array ( 'event_id', 'date', 'time', 'home', 'away', 'venue', 'day' );
-		$i = 1;
-		foreach ( $events as $event ) {
-			$events_array[$i]['event_id'] = $event->ID; //team_id
-			$events_array[$i]['date'] = get_the_date( 'Y/m/d', $event ); //date
-			$events_array[$i]['time'] = get_the_date( 'H:i:s', $event ); //time
-			$teams = get_post_meta ( $event->ID, 'sp_team' );
-			$events_array[$i]['home'] = get_the_title( $teams[0] ); //home
-			$events_array[$i]['away'] = get_the_title( $teams[1] ); //away
-			$venues = get_the_terms( $event->ID, 'sp_venue' );
-			if ( $venues ) {
-				$venue = $venues[0]->name;
-			}else{
-				$venue = '';
-			}
-			$events_array[$i]['venue'] = $venue; //venue
-			$events_array[$i]['day'] = get_post_meta ( $event->ID, 'sp_day', true ); //day
-			$i++;
-		}
-		foreach ( $events_array as $event_row ) {
-			fputcsv( $df, $event_row );
-		}
-		fclose( $df );
-		$export_data = ob_get_clean();
-		
-		header("Content-type: application/x-msdownload",true,200);
-		header("Content-Disposition: attachment; filename=data.csv");
-		header("Pragma: no-cache");
-		header("Expires: 0");
-		
-		echo $export_data;
-		
-		echo '</br>Exported ' . $number_of_events . ' events';
-		echo '</br><button type="button" onclick="history.back();">Go Back </button>';
-		exit;
-	}
-}
-?>
+ ?>
 <div class="wrap">
 	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 	<form method="post" action="<?php //echo esc_html( admin_url( 'admin-post.php' ) ); ?>">
@@ -91,10 +23,6 @@ if ( $_POST && isset( $_POST['submit'] ) ) {
 						'values' => 'slug',
 						'show_option_none' => __( '&mdash; Not set &mdash;', 'sportspress' ),
 					);
-					if ( ! sp_dropdown_taxonomies( $args ) ):
-						echo '<p>' . __( 'None', 'sportspress' ) . '</p>';
-						sp_taxonomy_adder( 'sp_league', 'sp_team', __( 'Add New', 'sportspress' ) );
-					endif;
 					?>
 				</label>
 			</li>
@@ -107,14 +35,10 @@ if ( $_POST && isset( $_POST['submit'] ) ) {
 						'values' => 'slug',
 						'show_option_none' => __( '&mdash; Not set &mdash;', 'sportspress' ),
 					);
-					if ( ! sp_dropdown_taxonomies( $args ) ):
-						echo '<p>' . __( 'None', 'sportspress' ) . '</p>';
-						sp_taxonomy_adder( 'sp_season', 'sp_team', __( 'Add New', 'sportspress' ) );
-					endif;
 					?>
 				</label>
 			</li>
 		</ul>
-		<?php submit_button( __( 'Export', 'textdomain' ) );?>
+		<?php submit_button( __( 'Export', 'sportspress' ) );?>
 	</form>
 </div>
