6cd735f410a6976aa3451b736bd16d77025852a8
savvasha <savvasha@hotmail.com>
2020-09-07 21:33:09 +0300

    Add fixtures filters do_action hook

--- includes/admin/exporters/class-sp-fixture-exporter.php
+++ includes/admin/exporters/class-sp-fixture-exporter.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 <div class="wrap">
 	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 	<form method="post" id="sp_fixture_export_form" action="">
-		<ul id="event-filters" class="export-filters" style="display: block;">
+		<ul id="fixtures-filters" class="export-filters" style="display: block;">
 			<li>
 				<label><span class="label-responsive"><?php _e( 'Format', 'sportspress' ); ?>&nbsp;</span>
 						<input type="radio" name="sp_format" class="post-format" id="post-format-league" value="league" checked="checked"> <label for="post-format-league" class="post-format-icon post-format-league"><?php _e( 'Competitive', 'sportspress' ); ?></label>
@@ -53,6 +53,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 					?>
 				</label>
 			</li>
+			<?php do_action('sportspress_fixtures_filters'); ?>
 		</ul>
 		<?php wp_nonce_field( 'sp-admin-exporters', 'sp_exporter_nonce' ); ?>
 		<?php submit_button( __( 'Export', 'sportspress' ) );?>
