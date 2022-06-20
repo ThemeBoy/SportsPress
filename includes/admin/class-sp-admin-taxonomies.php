d9767bdfd50ef8f01ca9ccebb0f2b4253fafed75
savvasha <savvasha@hotmail.com>
2022-06-20 21:29:51 +0300

    FIX: Can’t set Statistic variable for player positions

--- includes/admin/class-sp-admin-taxonomies.php
+++ includes/admin/class-sp-admin-taxonomies.php
@@ -267,7 +267,7 @@ class SP_Admin_Taxonomies {
 			$cat_keys  = array_keys( wp_unslash( $_POST['term_meta'] ) ); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput
 			foreach ( $cat_keys as $key ) {
 				if ( isset( $_POST['term_meta'][ $key ] ) ) {
-					$term_meta[ $key ] = sanitize_text_field( wp_unslash( $_POST['term_meta'][ $key ] ) );
+					$term_meta[ $key ] = wp_unslash( $_POST['term_meta'][ $key ] ); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput
 				}
 			}
 			update_option( "taxonomy_$t_id", $term_meta );
