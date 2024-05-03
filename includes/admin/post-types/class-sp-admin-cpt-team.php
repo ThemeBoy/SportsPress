9afe592a3eb585d6f73f5c161a396f656d338c01
savvasha <savvasha@hotmail.com>
2024-05-03 12:20:07 +0300

    FIX No2: preg_replace(): Passing null to parameter No3 ($subject) of type array|string is deprecated

--- includes/admin/post-types/class-sp-admin-cpt-team.php
+++ includes/admin/post-types/class-sp-admin-cpt-team.php
@@ -100,10 +100,12 @@ if ( ! class_exists( 'SP_Admin_CPT_Team' ) ) :
 					echo $abbreviation ? esc_html( $abbreviation ) : '&mdash;';
 					break;
 				case 'sp_league':
-					echo get_the_terms( $post_id, 'sp_league' ) ? wp_kses_post( the_terms( $post_id, 'sp_league' ) ) : '&mdash;';
+					$terms = get_the_terms( $post_id, 'sp_league' );
+					echo ( $terms && ! is_wp_error( $terms ) ) ? wp_kses_post( implode( ', ', wp_list_pluck( $terms, 'name' ) ) ) : '&mdash;';
 					break;
 				case 'sp_season':
-					echo get_the_terms( $post_id, 'sp_season' ) ? wp_kses_post( the_terms( $post_id, 'sp_season' ) ) : '&mdash;';
+					$terms = get_the_terms( $post_id, 'sp_season' );
+					echo ( $terms && ! is_wp_error( $terms ) ) ? wp_kses_post( implode( ', ', wp_list_pluck( $terms, 'name' ) ) ) : '&mdash;';
 					break;
 			endswitch;
 		}
