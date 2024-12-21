2339aa7c3d83d86a31d2771faecbe26be7b92bb3
savvasha <savvasha@hotmail.com>
2024-12-21 09:18:37 +0200

    FIX: A non-numeric value encountered

--- includes/class-sp-player-list.php
+++ includes/class-sp-player-list.php
@@ -629,6 +629,7 @@ class SP_Player_List extends SP_Secondary_Post {
 
 												// Subtract minutes prior to substitution
 												$substitution_time = (int) sp_array_value( sp_array_value( sp_array_value( sp_array_value( $timeline, $team_id ), $player_id ), 'sub' ), 0, 0 );
+												$played_minutes    = is_numeric($played_minutes) ? (int) $played_minutes : 0;
 												$played_minutes   -= $substitution_time;
 											else :
 
