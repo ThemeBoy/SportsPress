f78d28ba2fa230b1ba30535bfee0c433c7eea90e
Savvas <savvasha@hotmail.com>
2026-02-27 19:56:24 +0200

    FIX: preg_replace(): Passing null to parameter No3 ($subject) of type array|string is deprecated

--- includes/admin/post-types/class-sp-admin-cpt-table.php
+++ includes/admin/post-types/class-sp-admin-cpt-table.php
@@ -5,7 +5,7 @@
  * @author      ThemeBoy
  * @category    Admin
  * @package     SportsPress/Admin/Post_Types
- * @version     2.5
+ * @version     2.7.29
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -67,10 +67,12 @@ if ( ! class_exists( 'SP_Admin_CPT_Table' ) ) :
 		public function custom_columns( $column, $post_id ) {
 			switch ( $column ) :
 				case 'sp_league':
-					echo get_the_terms( $post_id, 'sp_league' ) ? wp_kses_post( the_terms( $post_id, 'sp_league' ) ) : '&mdash;';
+					$terms = get_the_terms( $post_id, 'sp_league' );
+					echo ( $terms && ! is_wp_error( $terms ) ) ? wp_kses_post( implode( ', ', wp_list_pluck( $terms, 'name' ) ) ) : '&mdash;';
 					break;
 				case 'sp_season':
-					echo get_the_terms( $post_id, 'sp_season' ) ? wp_kses_post( the_terms( $post_id, 'sp_season' ) ) : '&mdash;';
+					$terms = get_the_terms( $post_id, 'sp_season' );
+					echo ( $terms && ! is_wp_error( $terms ) ) ? wp_kses_post( implode( ', ', wp_list_pluck( $terms, 'name' ) ) ) : '&mdash;';
 					break;
 				case 'sp_team':
 					$select = get_post_meta( $post_id, 'sp_select', true );
