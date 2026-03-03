bd033b22f15b438e679967bb59bb6277dfd9d34e
savvasha <savvasha@hotmail.com>
2026-03-03 10:24:15 +0200

    First commit

---
+++ modules/sportspress-team-metrics.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Team Metrics
+ *
+ * @author    ThemeBoy
+ * @category  Modules
+ * @package   SportsPress/Modules
+ * @version   2.7.30
+ */
+
+ // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'SportsPress_Team_Metrics' ) ) :
+
+	/**
+	 * Main SportsPress Team Metrics Class
+	 *
+	 * @class SportsPress_Team_Metrics
+	 * @version 2.7.30
+	 */
+	class SportsPress_Team_Metrics {
+
+
+    }
+    
+endif;
+
+new SportsPress_Team_Metrics();
